2076f430b74b38ed646d95a646e48cbc500bebb6
SheikhMahin72 <mindsetmumentom@gmail.com>
2026-04-04 16:17:29 +0530

    add a first setup.py file

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = "-e ."
+
+def get_requirements(file_path: str) -> List[str]:
+
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.strip() for req in requirements]
+
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+
+    return requirements
+
+
+setup(
+    name="mlops_project",
+    version="0.1.0",
+    author="sheikh mahin",
+    author_email="mindsetmumentom@gmail.com",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=get_requirements("requirements.txt"),
+)
